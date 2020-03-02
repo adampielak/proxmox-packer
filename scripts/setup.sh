430e41aee33eff9c3ca68e353d18cae7b9deefe1
Stephen Rufle <srufle@gmail.com>
2020-03-01 23:07:03 -0700

    Added stop script

---
+++ scripts/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eux
+
+# Add sandbox user to sudoers.
+echo "sandbox        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+# sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+# Disable daily apt unattended updates.
+echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
+return 0
